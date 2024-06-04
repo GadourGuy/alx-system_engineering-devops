51e865ea5051f610c554dc46097951184dac2978
GadourGuy <ogadours1@example.com>
2024-06-04 19:19:02 +0800

    task 0 completed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,8 +15,8 @@ def number_of_subscribers(subreddit):
     """
     req = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
+        headers={"User-Agent": "Custom"}
+        )
 
     if req.status_code == 200:
         return req.json().get("data").get("subscribers")
